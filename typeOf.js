66c69a5410600c9a750303555fa09f40d4c50b10
Abdulhalim Yusuf <amilimos@hotmail.com>
2020-02-09 01:43:33 -0600

    Create typeOf.js

---
+++ typeOf.js
@@ -0,0 +1,32 @@
+// Number Data Type:
+var firstVar = 1.5e5; 
+
+// String Data Type:
+var secondVar = 'Hello'; 
+
+// Boolean Data Type:
+var thirdVar = true; 
+
+// Symbol Data Type:
+var fourthVar = Symbol("some Symbol variable"); 
+
+// Null Object:
+var fifthVar = null; 
+
+// Undefined Data Type:
+var sixthVar; 
+
+// Object:
+var seventhVar = {a: 1, b: 2}; 
+
+// NaN (It is a Number):
+var eighthVar = Math.sqrt(-1); 
+
+console.log(firstVar + " is a " + typeof firstVar);
+console.log(secondVar + " is a " + typeof secondVar);
+console.log(thirdVar + " is a " + typeof thirdVar);
+console.log(fourthVar.toString() + " is a " + typeof fourthVar);
+console.log(fifthVar + " is an " + typeof fifthVar);
+console.log(sixthVar + " is an " + typeof sixthVar);
+console.log(seventhVar + " is an " + typeof seventhVar);
+console.log(eighthVar + " is a " + typeof eighthVar);
